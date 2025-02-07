1334d70f4d290823d6842ce97321ace9937c9a82
DelTa-0 <aryalmadhave123@gmail.com>
2025-02-07 15:25:18 +0545

    signup form

--- marketplaceapp/core/views.py
+++ marketplaceapp/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from item.models import Category,Item
+from .forms import SignupForm,LoginFom
 # Create your views here.
 def index(request):
     items=Item.objects.filter(is_sold=False)[0:6]
@@ -9,3 +10,20 @@ def index(request):
 def contact(request):
     return render(request,'core/contact.html')
 
+def signup(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form=SignupForm()
+    return render(request,'core/signup.html',{
+        'form':form
+    })
+
+def login(request):
+    form=LoginFom()
+    return render(request,'core/login.html',{
+        'form':form
+    })
